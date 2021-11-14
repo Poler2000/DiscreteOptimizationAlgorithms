f2a7a4777672052ba9116e8978a979219112936d
poler <p.polerowicz@gmail.com>
2021-11-14 18:53:04 +0100

    implement 2 and 4 from list 2

--- L2/Zad2.jl
+++ L2/Zad2.jl
@@ -1,6 +1,7 @@
 using JuMP
 using GLPK
-
+using LinearAlgebra
+#=
 @variable(model, 0 <= x1)
 @variable(model, 0 <= y1)
 @variable(model, 0 <= z1)
@@ -50,3 +51,56 @@ optimize!(model)
 @show value(z2)
 @show value(z3)
 @show value(z4)
+=#
+f = open("data2.txt", "r")
+
+nStr = readline(f)
+mStr = readline(f)
+
+n = parse(Int64, nStr)
+m = parse(Int64, mStr)
+
+c = zeros(1, m)
+t = zeros(1, m)
+
+G = zeros(n, n)
+T = zeros(n, n)
+
+
+for i in 1:m
+    s = readline(f)
+    println(s)
+    path = parse.(Int32, split(s))
+    println(path)
+    x = path[1]
+    y = path[2]
+    G[x, y] = path[3]
+    T[x, y] = path[4]
+end
+
+println(G)
+println(T)
+
+println("Start the fun part!")
+
+shortest_path = Model(GLPK.Optimizer)
+
+@variable(shortest_path, x[1:n, 1:n], Bin)
+@constraint(shortest_path, [i = 1:n, j = 1:n; G[i, j] == 0], x[i, j] == 0)
+
+@constraint(
+    shortest_path,
+    [i = 1:n; i != 1 && i != 2],
+    sum(x[i, :]) == sum(x[:, i])
+)
+
+@constraint(shortest_path, sum(x[1, :]) - sum(x[:, 1]) == 1)
+
+@constraint(shortest_path, sum(x[2, :]) - sum(x[:, 2]) == -1)
+@constraint(shortest_path, sum(T[i,j]*x[i,j] for i in 1:n,j in 1:n) <= 5)
+@objective(shortest_path, Min, LinearAlgebra.dot(G, x))
+
+optimize!(shortest_path)
+objective_value(shortest_path)
+println(value.(x))
+println(sum(x))
